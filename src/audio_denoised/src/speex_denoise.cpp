37f31f1f12ba5f19422bb8dc5d61035755392842
saki <zxccid@hnu.edu.cn>
2017-03-17 16:25:56 +0800

    [添加speex降噪节点]

--- src/audio_denoised/src/speex_denoise.cpp
+++ src/audio_denoised/src/speex_denoise.cpp
@@ -1,9 +1,58 @@
 #include <ros/ros.h>
+#include <speex/speex_preprocess.h>
+#include <audio_msgs/AudioData.h>
+
+#define FRAME_SIZE 512
+#define SAMPLING_RATE 16000
+#define DB (-50) //maximun attenuation of the noise in db
+
+class Speex
+{
+public:
+  Speex()
+  {
+    _denoiseDB = DB;
+    _denoise_state = 1;
+
+    speex_preprocess_ctl(st, SPEEX_PREPROCESS_GET_DENOISE,&_denoise_state);
+    speex_preprocess_ctl(st, SPEEX_PREPROCESS_SET_NOISE_SUPPRESS, &_denoiseDB);
+
+    _pub = _nh.advertise<audio_msgs::AudioData>("denoised_data",1000);
+  }
+
+
+  static void denoiseCallback(const audio_msgs::AudioData &audio_data)
+  {
+    audio_msgs::AudioData denoise_data = audio_data;
+
+    speex_preprocess_run(st,(int16_t*)&denoise_data.data[0]);
+    _pub.publish(denoise_data);
+
+  }
+
+  void run_denoise()
+  {
+    _sb = _nh.subscribe("audio_data", 1000, denoiseCallback);
+
+    ros::spin();
+  }
+
+private:
+  int _denoise_state;
+  int _denoiseDB;
+  ros::NodeHandle _nh;
+  ros::Subscriber _sb;
+  static ros::Publisher _pub;
+  static SpeexPreprocessState *st;
+};
+
+ros::Publisher Speex::_pub;
+SpeexPreprocessState *Speex::st=speex_preprocess_state_init(FRAME_SIZE, SAMPLING_RATE);
+
 
 int main(int argc, char **argv)
 {
-  // Set up ROS.
   ros::init(argc, argv, "speex_denoise");
-
-  ROS_INFO("Hello world!");
+  Speex sp;
+  sp.run_denoise();
 }
