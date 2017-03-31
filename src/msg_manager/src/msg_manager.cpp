f8790d53a97701e4dbc9abe88ddbb2773c5d2e07
saki <zxccid@hnu.edu.cn>
2017-03-31 15:35:49 +0800

    messege control

---
+++ src/msg_manager/src/msg_manager.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+
+class msg_manager
+{
+public:
+
+
+private:
+
+};
+
+int main(int argc, char **argv)
+{
+  // Set up ROS.
+  ros::init(argc, argv, "msg_manager");
+
+  ROS_INFO("Hello world!");
+}
